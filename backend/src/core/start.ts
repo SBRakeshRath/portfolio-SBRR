dcc6a2638ee9577e1f3b38f0e75e89547965aab0
S B Rakesh Rath <work.sbrakeshrath@gmail.com>
2024-06-13 13:19:27 +0530

    fixed port issue for backend

--- backend/src/core/start.ts
+++ backend/src/core/start.ts
@@ -8,9 +8,9 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-const port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server is running on port 3000');
 });
 
