b01379d9b238925521fbe779a6acb5a0088f4b1d
S B Rakesh Rath <work.sbrakeshrath@gmail.com>
2024-06-13 12:08:59 +0530

    created a basic express server

--- backend/src/core/start.ts
+++ backend/src/core/start.ts
@@ -1,4 +1,20 @@
-//hello world
-import func from "../functions/fum.js";
+import Express from 'express';
+import dotenv from 'dotenv';
+dotenv.config()
 
-console.log(func());
+const app = Express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+
+
+
+
