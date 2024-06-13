2f5e5d46cb4c5fdd31d4b7964d902c56f3b7a1d9
S B Rakesh Rath <work.sbrakeshrath@gmail.com>
2024-06-13 14:30:12 +0530

    created a temporary template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <h1>S B Rakesh Rath portfolio is under construction.</h1>
+      <h3>If you still want to connect please contact him by:</h3>
+      <p>
+        Email:{" "}
+        <a href="mailto:work.sbrakeshrath@gmail.com">
+          {" "}
+          work.sbrakeshrath@gmail.com{" "}
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      </p>
+      <p>
+        Phone: <a href="tel:+919739112345"> +91 9337691832</a>
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
